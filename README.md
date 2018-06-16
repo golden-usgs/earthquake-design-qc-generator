e35295496ab7f032ae6445a3ce264fada15cbda7
jcaple <capable2@gmail.com>
2018-06-16 14:26:45 -0400

    initial

---
+++ README.md
@@ -0,0 +1,14 @@
+# QC Generator
+
+## Description
+
+Python script to read CSV qc files for the generation of QC JSON for use in the testing of
+earthquake-design-ws web services.
+
+[See the earthquake-design-ws project on GitHub](https://github.com/usgs/earthquake-design-ws)
+
+## Running
+
+1.  Edit the files at the top of the python script
+1.  Run the python script
+1.  ```generate-qc.py > expections.json```
---
+++ generate-qc.py
@@ -0,0 +1,119 @@
+import json
+
+path="/path/to/qc/"
+
+files = {
+    "Batch File Request for Alaska off grid Site Class A.csv": "Batch Request Results for Alaska off grid Site Class A.csv",
+    "Batch File Request for Alaska off grid Site Class B.csv": "Batch Request Results for Alaska off grid Site Class B.csv",
+    "Batch File Request for Alaska off grid Site Class C.csv": "Batch Request Results for Alaska off grid Site Class C.csv",
+    "Batch File Request for Alaska off grid Site Class D.csv": "Batch Request Results for Alaska off grid Site Class D.csv",
+    "Batch File Request for Alaska off grid Site Class E.csv": "Batch Request Results for Alaska off grid Site Class E.csv",
+    "Batch File Request for ConUS off grid Site Class A.csv": "Batch Request Results for ConUS off grid Site Class A.csv",
+    "Batch File Request for ConUS off grid Site Class B.csv": "Batch Request Results for ConUS off grid Site Class B.csv",
+    "Batch File Request for ConUS off grid Site Class C.csv": "Batch Request Results for ConUS off grid Site Class C.csv",
+    "Batch File Request for ConUS off grid Site Class D.csv": "Batch Request Results for ConUS off grid Site Class D.csv",
+    "Batch File Request for ConUS off grid Site Class E.csv": "Batch Request Results for ConUS off grid Site Class E.csv",
+    "Batch File Request for Hawaii off grid Site Class A.csv": "Batch Request Results for Hawaii off grid Site Class A.csv",
+    "Batch File Request for Hawaii off grid Site Class B.csv": "Batch Request Results for Hawaii off grid Site Class B.csv",
+    "Batch File Request for Hawaii off grid Site Class C.csv": "Batch Request Results for Hawaii off grid Site Class C.csv",
+    "Batch File Request for Hawaii off grid Site Class D.csv": "Batch Request Results for Hawaii off grid Site Class D.csv",
+    "Batch File Request for Hawaii off grid Site Class E.csv": "Batch Request Results for Hawaii off grid Site Class E.csv",
+    "Batch File Request for PRVI off grid Site Class A.csv": "Batch Request Results for PRVI off grid Site Class A.csv",
+    "Batch File Request for PRVI off grid Site Class B.csv": "Batch Request Results for PRVI off grid Site Class B.csv",
+    "Batch File Request for PRVI off grid Site Class C.csv": "Batch Request Results for PRVI off grid Site Class C.csv",
+    "Batch File Request for PRVI off grid Site Class D.csv": "Batch Request Results for PRVI off grid Site Class D.csv",
+    "Batch File Request for PRVI off grid Site Class E.csv": "Batch Request Results for PRVI off grid Site Class E.csv"
+} 
+
+qc_arr = []
+
+for key, value in files.items():
+    #print key + " " + value
+
+    key_lines = []
+    value_lines = []
+
+    file = open(path+key, "r")
+    for line in file:
+        # print line
+        key_lines.append(line)
+
+    file = open(path+value, "r")
+    for line in file:
+        value_lines.append(line)
+
+    # Iterate over arrays
+    for kl in key_lines:
+
+        qc = {
+            "request": {
+                "referenceDocument": "AASHTO-2009",
+                "status": "success",
+                "url": "",
+                "parameters": {
+                    "latitude": 0,
+                    "longitude": 0,
+                    "riskCategory": "I",
+                    "title": "",
+                    "siteClass": ""
+                }
+            },
+            "response": {
+                "data": {
+                    "pga": 0,
+                    "fpga": 0,
+                    "as": 0,
+                    "sds": 0
+                }
+            }
+        }
+
+        key_values = kl.split(",")
+        #print values[0].strip() + "*" + values[1].strip()
+        key_lat = key_values[0].strip()
+        key_long = key_values[1].strip()
+        key_class = key_values[2].strip()
+        
+        key_class_s = ""
+        if (int(key_class) == 0):
+            key_class_s = "A"
+        if (int(key_class) == 1):
+            key_class_s = "B"
+        if (int(key_class) == 2):
+            key_class_s = "C"
+        if (int(key_class) == 3):
+            key_class_s = "D"
+        if (int(key_class) == 4):
+            key_class_s = "E"
+
+        key_area = key_values[4].strip()
+
+        #print key_area
+
+        for vl in value_lines:
+            value_values = vl.split(",")
+            value_lat = value_values[0].strip()
+            value_long = value_values[1].strip()
+
+            if ( (key_lat == value_lat) and (key_long == value_long) ):
+                value_sc = value_values[2]
+                value_pga = value_values[3]
+                value_fpga = value_values[6]
+                value_as = value_values[9]
+                value_sds = value_values[10]
+
+                #print value_lat + "," + value_long + "," + key_area + "," + key_class_s + "," + value_pga + "," + value_fpga + "," + value_as + "," + value_sds
+
+                qc["request"]["parameters"]["latitude"] = value_lat
+                qc["request"]["parameters"]["longitude"] = value_long
+                qc["request"]["parameters"]["title"] = key_area
+                qc["request"]["parameters"]["siteClass"] = key_class_s
+                qc["response"]["data"]["pga"] = value_pga
+                qc["response"]["data"]["fpga"] = value_fpga
+                qc["response"]["data"]["as"] = value_as
+                qc["response"]["data"]["sds"] = value_sds
+
+                qc_arr.append(qc)
+
+print(json.dumps(qc_arr, indent=4, sort_keys=False))
+#print "Records: " + str(len(qc_arr))
